1b139fc775dd1d0e4ce5ca3329863875f06ee354
deepakshimpi001 <deepakshimpi001@gmail.com>
2023-04-02 10:42:19 +0530

    new os module file added

---
+++ import os.py
@@ -0,0 +1,18 @@
+import os
+
+#get the current directory
+print("*************************")
+print(os.getcwd())
+
+#for creating the directory and folder
+
+#os.mkdir('parentdir')
+
+#os.mkdir('parentdir/childdir')
+
+#if parent folder not created then we can't create the subfolder by using the this method os.mkdir() for that we use another method os.makedirs()
+
+
+os.makedirs("ds\df\gh\hj")
+
+
